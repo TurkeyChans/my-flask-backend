0e236849278af8ea9a7099c340f4a0d6d0e0ca84
TurkeyChans <58515738+TurkeyChans@users.noreply.github.com>
2025-06-25 01:00:59 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=os.environ['DB_HOST'],
+        port=os.environ.get('DB_PORT', 5432),
+        dbname=os.environ['DB_NAME'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASS']
+    )
+    return conn
+
+@app.route('/data')
+def get_data():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM your_table") 
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(rows)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
